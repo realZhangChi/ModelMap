807f4fe1423ab744a13a99e54e1cf4c17e5b037e
Zhang Chi <zliming@outlook.com>
2021-06-02 17:29:45 +0800

    添加项目文件。

---
+++ ModelMap/Startup.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Maui;
+using Microsoft.Maui.Controls.Compatibility;
+using Microsoft.Maui.Hosting;
+using Microsoft.AspNetCore.Components.WebView.Maui;
+using ModelMap.Data;
+using Microsoft.Maui.Controls.Hosting;
+
+namespace ModelMap
+{
+    public class Startup : IStartup
+    {
+        public void Configure(IAppHostBuilder appBuilder)
+        {
+            appBuilder
+                .UseMauiControlsHandlers()
+                .UseFormsCompatibility()
+                .RegisterBlazorMauiWebView(typeof(Startup).Assembly)
+                .UseMicrosoftExtensionsServiceProviderFactory()
+                .UseMauiApp<App>()
+                .ConfigureFonts(fonts =>
+                {
+                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                })
+                .ConfigureServices(services =>
+                {
+                    services.AddBlazorWebView();
+                    services.AddSingleton<WeatherForecastService>();
+                });
+        }
+    }
+}
